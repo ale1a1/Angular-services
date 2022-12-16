b94af45f3bbaf56a544f38d6503e0426b8882f32
alessandro ladu <ale1a@libero.it>
2022-12-16 16:40:23 +0000

    inject LoggingService into AccountService

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -6,19 +6,14 @@ import { LoggingService } from '../logging.service';
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers: [LoggingService],
 })
 export class AccountComponent {
   @Input() account: { name: string; status: string };
   @Input() id: number;
 
-  constructor(
-    private loggingService: LoggingService,
-    private accountService: AccountService
-  ) {}
+  constructor(private accountService: AccountService) {}
 
   onSetTo(status: string) {
-    this.loggingService.logStatusChange(status);
     this.accountService.updateStatus(this.id, status);
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from './account.service';
+import { LoggingService } from './logging.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [AccountService],
+  providers: [],
 })
 export class AppComponent implements OnInit {
   accounts: { name: string; status: string }[];
